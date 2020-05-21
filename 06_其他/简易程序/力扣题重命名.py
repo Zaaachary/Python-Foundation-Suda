1f3b246188bc1be6e707a8784f518ef6d9bce3f9
Zachary Lee <li.zachary@gmail.com>
2020-05-21 12:11:51 +0800

    编写改名程序 完成力扣题目重命名

--- 06_其他/简易程序/力扣题重命名.py
+++ 06_其他/简易程序/力扣题重命名.py
@@ -1,4 +1,40 @@
 """
 Leetcode 文件夹中的文件序号 在小于4位数的序号前面补零
+
 """
 import os
+import re
+
+class FileRenamer:
+    def __init__(self, targetdir='./'):
+        self.targetdir = targetdir
+        os.chdir(targetdir)
+        print('Now working at ' + os.getcwd())
+
+    def changedir(self, targetdir):
+        print("Changing working directory")
+        self.__init__(targetdir)
+
+    def renamenumber(self, length):
+        """
+        length: 目标数字序号长度
+        """
+        pattern = re.compile(r'^\d+')
+        for filename in os.listdir():
+            if number := pattern.match(filename):   # 海象运算符
+                number = number.group()
+            else:
+                continue
+
+            characters = filename[len(number):]
+            num = int(number)
+            number = str(num).rjust(length, '0')
+            print(dst:=number+characters)
+
+            os.rename(src=filename, dst=dst)
+        print("改名完成")
+
+if __name__ == "__main__":
+    F = FileRenamer()
+    F.changedir('./05_leetcode')
+    F.renamenumber(4)
