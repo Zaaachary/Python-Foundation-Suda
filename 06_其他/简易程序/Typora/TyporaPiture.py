e68462265c98d97b9e9d3140f2c85fe0bfc554c7
Zachary Lee <li_zaaachary@163.com>
2020-11-06 10:59:04 +0800

    leetcode weekly

--- 06_其他/简易程序/Typora/TyporaPiture.py
+++ 06_其他/简易程序/Typora/TyporaPiture.py
@@ -7,6 +7,8 @@
 此程序正是用来应对 typora 的图片存储问题 程序主要实现了以下功能
 1. 将目标目录中的 .md 中存于 其他目录的图片 复制到 目标目录下的图片目录
 2. 清理目标目录下的图片目录 删除 .md 文件未使用的图片
+
+3. 重命名某个 .md 文件的图片名称，并更改 md 内的链接
 """
 import os
 import sys
@@ -20,12 +22,25 @@ class Cleaner:
         os.chdir(targetdir)
         print('Now working at ' + os.getcwd())
 
+        self.current_picture = list()
+
     def changedir(self, targetdir):
         print("Changing working directory")
         self.__init__(targetdir)
 
-    def walk_through(self):
+    def get_pic_names(self):
+        # self.current_picture.append()
         pass
+
+    def walk_through(self):
+        w = os.walk(self.targetdir)
+        for a,b,c in w:
+            self.get_pic_names()
+            # TO-DO
+            self.current_picture.clear()
+
+
+
 
 
 if __name__ == "__main__":
